3909aa7d24724f187600b4dd34b0cee51cb9bed1
eestelle <yosiky@list.ru>
2025-02-11 18:54:03 +0300

    Step1: Do all exercises
    Signed-off-by: eestelle <yosiky@list.ru>

--- cmake-3.31.5-tutorial-source/Step1/tutorial.cxx
+++ cmake-3.31.5-tutorial-source/Step1/tutorial.cxx
@@ -1,13 +1,15 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
+// #include <cstdlib> // TODO 5: Remove this line
 #include <iostream>
 #include <string>
 
 // TODO 11: Include TutorialConfig.h
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
+  std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
   if (argc < 2) {
     // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
     //          and Tutorial_VERSION_MINOR
@@ -17,7 +19,7 @@ int main(int argc, char* argv[])
 
   // convert input to double
   // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  const double inputValue = atof(argv[1]);
+  const double inputValue = std::stod(argv[1]);
 
   // calculate square root
   const double outputValue = sqrt(inputValue);
